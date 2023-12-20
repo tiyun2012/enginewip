8d8c6c828097c479cdf6f6be22606af7018d8be8
Nhat <hoathinhvietnam2016@gmail.com>
2023-12-20 16:04:26 +0700

    adjust engineMain add glfwterminate before return in main()

--- src/engineMain.cpp
+++ src/engineMain.cpp
@@ -1,71 +1,98 @@
-#include<iostream>
-//#include<Windows.h>
-#include<glad\glad.h>
-#include<GLFW\glfw3.h>
-//color consol
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// ImGui includes
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+// Color constants for console output
 #define RED_TEXT "\033[31m"
 #define GREEN_TEXT "\033[32m"
 #define RESET_TEXT "\033[0m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[96m"
-
+//GLFW
 GLFWwindow* window;
-std::string* nameA;
+//imgui
+int sliderValue = 0;
 
-
-int main()
-{
-	//Set Error callback
-	glfwSetErrorCallback(errorCallback);
-	// check if glfwInit
-	if (!glfwInit())
-	{
-		std::cerr << "failed to initialize window";
-		return -1;
-	}
-	else
-	{
-		std::cout <<GREEN_TEXT<< "glfwInit works"<<RESET_TEXT<<std::endl;
-	}
-	// Set GLFW to not create an OpenGL context (we will use glad to load the OpenGL functions)
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow(800, 600, "My App", nullptr, nullptr);
-	if (!window)
-	{
-		std::cerr << "failed to initial GlFW";
-		// cleanup and deallocation, window and context destruction, even system clean up
-		glfwTerminate();
-		return -1;
-	}
-	//make the window's context current
-	glfwMakeContextCurrent(window);
-	//load openGl function using glad
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cerr << "Failed to initialize glad" << std::endl;
-	}
-	//mainloop
-	int numbers = 0;
-	while (!glfwWindowShouldClose(window))
-	{
-		//update
-		std::cout << numbers++ << std::endl;
-
-
-		// Swap front and back buffers
-		glfwSwapBuffers(window);
-
-		// Poll for and process events
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-		return 0;
+void errorCallback(int error, const char* description) {
+    std::cerr << RED_TEXT << "Error: " << error << " : " << description << RESET_TEXT << std::endl;
 }
 
-void errorCallback(int error, const char* description)
-{
-	std::cerr << "error: " << error << " : " << description << std::endl;
+int main() {
+
+    // trace glfw error
+
+    glfwSetErrorCallback(errorCallback);
+
+    if (!glfwInit()) {
+        std::cerr << RED_TEXT << "Failed to initialize GLFW" << RESET_TEXT << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "ImGui + GLFW App", nullptr, nullptr);
+    if (!window) {
+        std::cerr << RED_TEXT << "Failed to create GLFW window" << RESET_TEXT << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << RED_TEXT << "Failed to initialize GLAD" << RESET_TEXT << std::endl;
+        return -1;
+    }
+    // Get the OpenGL version
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    std::cout << "Renderer: " << renderer << std::endl;
+    std::cout << "OpenGL version supported: " << version << std::endl;
+
+    // ImGui initialization
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // ImGui content
+        ImGui::Begin("Demo window");
+        ImGui::SliderInt("Slider", &sliderValue, 0, 100);
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
