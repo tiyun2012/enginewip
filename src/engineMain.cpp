c4d6bed3e95f12104abefc2f143862b7dfc0ebce
Nhat <hoathinhvietnam2016@gmail.com>
2023-12-24 21:56:30 +0700

    add menu:             if (ImGui::BeginMainMenuBar())             {                 if (ImGui::BeginMenu("File"))                 {                     if (ImGui::MenuItem("Open..", "Ctrl+O")) { }                     if (ImGui::MenuItem("Save", "Ctrl+S")) { }                     if (ImGui::MenuItem("Close", "Ctrl+W"))  { glfwSetWindowShouldClose(window, 1); }                     ImGui::EndMenu();                 }                 ImGui::EndMainMenuBar();             }

--- src/engineMain.cpp
+++ src/engineMain.cpp
@@ -7,31 +7,40 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#include <string>
+#include <fstream>
+#include <sstream>
+
 // Color constants for console output
 #define RED_TEXT "\033[31m"
 #define GREEN_TEXT "\033[32m"
 #define RESET_TEXT "\033[0m"
-//GLFW
-GLFWwindow* window;
-//imgui
 
+GLFWwindow* window;
+
+// Function prototypes
 void errorCallback(int error, const char* description);
 void buttonCall();
+std::string readShaderSource(const char* filePath);
+GLuint compileShader(GLenum type, const std::string& source);
+GLuint createShaderProgram(const char* vertexFilePath, const char* fragmentFilePath);
+
 int main() {
-
-    // trace glfw error
-
+    // Error callback for GLFW
     glfwSetErrorCallback(errorCallback);
 
+    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << RED_TEXT << "Failed to initialize GLFW" << RESET_TEXT << std::endl;
         return -1;
     }
 
+    // Configure GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Create a GLFW window
     window = glfwCreateWindow(800, 600, "ImGui + GLFW App", nullptr, nullptr);
     if (!window) {
         std::cerr << RED_TEXT << "Failed to create GLFW window" << RESET_TEXT << std::endl;
@@ -39,19 +48,22 @@ int main() {
         return -1;
     }
 
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
+    // Load OpenGL functions using GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << RED_TEXT << "Failed to initialize GLAD" << RESET_TEXT << std::endl;
         return -1;
     }
-    // Get the OpenGL version
-    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-    const GLubyte* version = glGetString(GL_VERSION); // version as a string
-    std::cout <<GREEN_TEXT<< "Renderer: " << RED_TEXT << renderer << std::endl;
-    std::cout <<GREEN_TEXT<< "OpenGL version supported: " <<RED_TEXT<< version << RESET_TEXT<<std::endl;
 
-    // ImGui initialization
+    // Get and print the OpenGL version
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* version = glGetString(GL_VERSION);
+    std::cout << GREEN_TEXT << "Renderer: " << renderer << std::endl;
+    std::cout << "OpenGL version supported: " << version << RESET_TEXT << std::endl;
+
+    // Initialize ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -59,10 +71,39 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
-    //imgui variable initialization
-    
-    
-    
+    // Shader setup
+    GLuint shaderProgram = createShaderProgram("src/vertex_shader.glsl", "src/fragment_shader.glsl");
+
+    // Rectangle setup
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f  // top left 
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
@@ -71,102 +112,166 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // ImGui content
+        // ImGui content here
+        // ...
         ImGui::Begin("Demo window");
-            // menu
-            if (ImGui::BeginMainMenuBar())
+        // menu
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
             {
-                if (ImGui::BeginMenu("File"))
-                {
-                    if (ImGui::MenuItem("Open..", "Ctrl+O")) { }
-                    if (ImGui::MenuItem("Save", "Ctrl+S")) { }
-                    if (ImGui::MenuItem("Close", "Ctrl+W"))  { glfwSetWindowShouldClose(window, 1); }
-                    ImGui::EndMenu();
-                }
-                ImGui::EndMainMenuBar();
+                if (ImGui::MenuItem("Open..", "Ctrl+O")) {}
+                if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+                if (ImGui::MenuItem("Close", "Ctrl+W")) { glfwSetWindowShouldClose(window, 1); }
+                ImGui::EndMenu();
             }
+            ImGui::EndMainMenuBar();
+        }
 
-             static int sliderValue = 10;
-            ImGui::SliderInt("Slider", &sliderValue, 0, 100);
-            static float f = 0.5;
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-            const static int intTextA = 1995;
-            ImGui::Text("Hello, world %d", intTextA);
-            //const static float spacing = 4;
-            //ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(spacing, spacing));
+        static int sliderValue = 10;
+        ImGui::SliderInt("Slider", &sliderValue, 0, 100);
+        static float f = 0.5;
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+        const static int intTextA = 1995;
+        ImGui::Text("Hello, world %d", intTextA);
+        //const static float spacing = 4;
+        //ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(spacing, spacing));
 
-            ImGui::SameLine(0.0f,-10.0f);
-            if(ImGui::Button("run"))buttonCall();
-            // Color buttons, demonstrate using PushID() to add unique identifier in the ID stack, and changing style.
-            //IMGUI_DEMO_MARKER("Widgets/Basic/Buttons (Colored)");
-            for (int i = 0; i < 7; i++)
-            {
-                if (i > 0)
-                    ImGui::SameLine();
-                ImGui::PushID(i);
-                ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(i / 7.0f, 0.6f, 0.6f));
-                ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(i / 7.0f, 0.7f, 0.7f));
-                ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(i / 7.0f, 0.8f, 0.8f));
-                ImGui::Button("Click");
-                ImGui::PopStyleColor(3);
-                ImGui::PopID();
-            }
+        ImGui::SameLine(0.0f, -10.0f);
+        if (ImGui::Button("run"))buttonCall();
+        // Color buttons, demonstrate using PushID() to add unique identifier in the ID stack, and changing style.
+        //IMGUI_DEMO_MARKER("Widgets/Basic/Buttons (Colored)");
+        for (int i = 0; i < 7; i++)
+        {
+            if (i > 0)
+                ImGui::SameLine();
+            ImGui::PushID(i);
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(i / 7.0f, 0.6f, 0.6f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(i / 7.0f, 0.7f, 0.7f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(i / 7.0f, 0.8f, 0.8f));
+            ImGui::Button("Click");
+            ImGui::PopStyleColor(3);
+            ImGui::PopID();
+        }
 
-            // Set the item spacing to 10 pixels in both x and y directions
-            const static float spacing = 20;
-            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(sliderValue,  spacing));
+        // Set the item spacing to 10 pixels in both x and y directions
+        const static float spacing = 20;
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(sliderValue, spacing));
 
-            // Now create some widgets
-            if (ImGui::Button("Button 1"))
-            {
-                // Handle button click
-            }
+        // Now create some widgets
+        if (ImGui::Button("Button 1"))
+        {
+            // Handle button click
+        }
 
-            ImGui::SameLine();
+        ImGui::SameLine();
 
-            if (ImGui::Button("Button 2"))
-            {
-                // Handle button click
-            }
+        if (ImGui::Button("Button 2"))
+        {
+            // Handle button click
+        }
 
-            // Don't forget to pop the style variable when you're done with it!
-            ImGui::PopStyleVar();
+        // Don't forget to pop the style variable when you're done with it!
+        ImGui::PopStyleVar();
 
         //->end
         ImGui::End();
-
-        // Rendering
-        //  ---Imgui
+        // Render ImGui
         ImGui::Render();
-        //  ---glfw
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
+
+        // Clear the screen
         glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Render rectangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Render ImGui draw data
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+        // Swap front and back buffers
         glfwSwapBuffers(window);
     }
 
     // Cleanup
-    //   --imgui
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
-    //   --glfw
+
     glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
 }
 
-
 void errorCallback(int error, const char* description) {
     std::cerr << RED_TEXT << "Error: " << error << " : " << description << RESET_TEXT << std::endl;
 }
-void buttonCall()
-{
-    std::cout << "run button calling" << std::endl;
-};
+
+void buttonCall() {
+    std::cout << "Run button pressed" << std::endl;
+}
+
+std::string readShaderSource(const char* filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
+
+    if (!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
+
+    std::string line = "";
+    while (!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
+
+GLuint compileShader(GLenum type, const std::string& source) {
+    GLuint shader = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(shader, 1, &src, NULL);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    return shader;
+}
+
+GLuint createShaderProgram(const char* vertexFilePath, const char* fragmentFilePath) {
+    std::string vertexCode = readShaderSource(vertexFilePath);
+    std::string fragmentCode = readShaderSource(fragmentFilePath);
+
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexCode);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentCode);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
